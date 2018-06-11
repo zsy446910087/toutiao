a1c258d9397d33c101d0d78a908389ee69a6a8c0
zsy446910087 <446910087@qq.com>
2018-06-11 09:51:49 +0800

    first  commit service

---
+++ src/main/java/com/newcoder/toutiao/service/ToutiaoService.java
@@ -0,0 +1,14 @@
+package com.newcoder.toutiao.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author:Siyu
+ * @Date:Created in 上午9:49 2018/6/11
+ */
+@Service
+public class ToutiaoService {
+    public String say() {
+        return "This is from ToutiaoService";
+    }
+}
